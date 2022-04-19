eabf8a89c42d311b2dba3c189e43e83eb14d914f
Dattaraj <dattaraj@nimapinfotech.com>
2022-04-19 11:33:24 +0530

    Removed Unwanted Code and files

--- admin/registeredlist.php
+++ admin/registeredlist.php
@@ -13,7 +13,7 @@
 <?php include 'components/header.php';?>
 <div class="container-fluid">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Approval List <?php echo (isset($data_rescuer['rows'])) ? '('.count($data_rescuer['rows']).')':'';?></h1>
+        <h1 class="h3 mb-0 text-gray-800">Request List <?php echo (isset($data_rescuer['rows'])) ? '('.count($data_rescuer['rows']).')':'';?></h1>
     </div>
    	<!-- DataTales Example -->
     <div class="card shadow mb-4">
